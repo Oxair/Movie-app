2be3ae04e7f42d877a2af83f61245c97b00b0a89
Oxair <pk.ozair@gmail.com>
2019-09-25 11:23:56 +0500

    Stage 4

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 // To se the latest configuration fetch it from https://api.themoviedb.org/3/configuration?api_key=019e8f375549e0bbd4a4191862ebc88f
 
 const API_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = '844dba0bfd8f3a4f3799f6130ef9e335';
+const API_KEY = 'b46f0240ee193e21a1661dd335e1a0d2';
 
 // Images
 // An image URL looks like this example:
